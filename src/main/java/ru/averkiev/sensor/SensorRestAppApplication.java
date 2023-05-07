defc7385e43ee22959bd5efdcd76f608e90a2ef5
”MrGreenNV” <averkievnv@gmail.com>
2023-05-07 23:23:21 +0300

    init

---
+++ src/main/java/ru/averkiev/sensor/SensorRestAppApplication.java
@@ -0,0 +1,13 @@
+package ru.averkiev.sensor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SensorRestAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SensorRestAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/averkiev/sensor/SensorRestAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.averkiev.sensor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SensorRestAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
