c90c90aff8de3151c975a14107d69b9ee6ff9c59
MrGreenNV <averkievnv@gmail.com>
2023-07-01 23:06:30 +0300

    add SensorService.java

---
+++ src/main/java/ru/averkiev/sensor/services/SensorService.java
@@ -0,0 +1,25 @@
+package ru.averkiev.sensor.services;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ru.averkiev.sensor.models.Sensor;
+import ru.averkiev.sensor.repositories.SensorRepository;
+
+import java.util.Optional;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class SensorService {
+
+    private final SensorRepository sensorRepository;
+
+    public void register(Sensor sensor) {
+        sensorRepository.save(sensor);
+    }
+
+    public Optional<Sensor> findByName(String name) {
+        return sensorRepository.findByName(name);
+    }
+}
