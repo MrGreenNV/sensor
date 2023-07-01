2ae57dafa65f17e7fc365a20a76683bdb7e58e45
MrGreenNV <averkievnv@gmail.com>
2023-07-01 22:23:19 +0300

    update Sensor.java

--- src/main/java/ru/averkiev/sensor/models/Sensor.java
+++ src/main/java/ru/averkiev/sensor/models/Sensor.java
@@ -3,10 +3,18 @@ package ru.averkiev.sensor.models;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
 
 @Entity
 @Table(name = "Sensor")
-public class Sensor {
+@Getter
+@Setter
+@RequiredArgsConstructor
+public class Sensor implements Serializable {
 
     @Id
     @Column(name = "id")
@@ -17,27 +25,4 @@ public class Sensor {
     @NotEmpty(message = "Название сенсора не должно быть пустым!")
     @Size(min = 3, max = 30, message = "Название сенсора должно быть от 3 до 30 символов!")
     private String name;
-
-    public Sensor() {
-    }
-
-    public Sensor(String name, double temperature, boolean raining) {
-        this.name = name;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
