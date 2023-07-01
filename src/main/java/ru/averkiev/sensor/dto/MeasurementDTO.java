01b71e235fc934be39bbfe7333d4e19a02793bc9
MrGreenNV <averkievnv@gmail.com>
2023-07-02 00:03:34 +0300

    add MeasurementDTO.java

---
+++ src/main/java/ru/averkiev/sensor/dto/MeasurementDTO.java
@@ -0,0 +1,25 @@
+package ru.averkiev.sensor.dto;
+
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@RequiredArgsConstructor
+public class MeasurementDTO {
+
+    @NotNull
+    @Min(-100)
+    @Max(100)
+    private Double value;
+
+    @NotNull
+    private Boolean isRaining;
+
+    @NotNull
+    private SensorDTO sensor;
+}
