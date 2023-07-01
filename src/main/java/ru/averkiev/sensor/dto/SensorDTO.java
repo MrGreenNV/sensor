3897e9bacc43b70a8058d87f85c1b8c9134a3299
MrGreenNV <averkievnv@gmail.com>
2023-07-01 22:56:28 +0300

    add SensorDTO.java

---
+++ src/main/java/ru/averkiev/sensor/dto/SensorDTO.java
@@ -0,0 +1,16 @@
+package ru.averkiev.sensor.dto;
+
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@RequiredArgsConstructor
+public class SensorDTO {
+    @NotEmpty(message = "Название не должно быть пустым!")
+    @Size(min = 3, max = 30, message = "Название сенсора должно быть от 3 до 30 символов!")
+    private String name;
+}
